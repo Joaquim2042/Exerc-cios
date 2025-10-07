d712674b4655b92bea72edbdf897cc7e986c891e
Joaquim2042 <joaquimdeoliveirasilva47@gmail.com>
2025-10-07 16:00:06 -0300

    Add files via upload

---
+++ Questão 18/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿double num = 1;
+double media = 1;
+
+for (int i = 0; i < media; i++)
+{
+    if (num != 0)
+    {
+        Console.WriteLine("Digite um número.");
+        num = double.Parse(Console.ReadLine());
+    }
+    if (num != 0)
+    {
+        media = num / 2;
+        Console.WriteLine(media);
+    }
+}
